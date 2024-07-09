e7d161d5f27641c2443fa6f179c98c63906aacf4
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-07-09 23:03:02 +0300

    CodeFirst Migration ( remove-migration / script-migration)

--- Section03.CodeFirst/Program.cs
+++ Section03.CodeFirst/Program.cs
@@ -5,6 +5,10 @@ using Section03.CodeFirst.DAL;
 // add-migration initial => Migration oluşturur
 // update-database => Migration'ı database'e uygular.
 
+//Remove-Migration => Son migration'ı siler.
+//update-database UpdateBarcode_ForProduct => seçilen migration'a döner
+//script-migration => yapılan tüm değişikliklerin sql sorgusunu verir
+
 Initializer.Build();
 
 using (var _context = new AppDbContext())
