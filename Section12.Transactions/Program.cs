86e460328098d853948a24d988ef942ef11e9c0f
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-20 20:12:13 +0300

    SaveChanges()

---
+++ Section12.Transactions/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Section12.Transactions.DAL;
+
+using (var _context = new AppDbContext())
+{
+
+    var category = new Category() { Name = "Telefonlar" };
+
+    _context.Categories.Add(category);
+
+
+    throw new Exception();
+
+
+    var product = _context.Products.First();
+
+    product.Name = "Defter 123";
+
+
+    _context.SaveChanges();
+
+    Console.WriteLine();
+}
