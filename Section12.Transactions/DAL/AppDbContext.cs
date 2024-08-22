4ca34f53905d526c0f4d61edc41c0e05096f24d6
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-22 22:04:46 +0300

    Multiple DbContext Instance

--- Section12.Transactions/DAL/AppDbContext.cs
+++ Section12.Transactions/DAL/AppDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace Section12.Transactions.DAL
 {
     public class AppDbContext : DbContext
     {
+        private DbConnection _connection;
+
+        public AppDbContext(DbConnection connection)
+        {
+            _connection = connection;
+        }
+        public AppDbContext()
+        {
+            
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -17,8 +29,15 @@ namespace Section12.Transactions.DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            Initializer.Build();
-            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+            if (_connection is default(DbConnection))
+            {
+                Initializer.Build();
+                optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+            }
+            else
+            {
+                optionsBuilder.UseSqlServer(_connection);
+            }    
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Section12.Transactions/Program.cs
+++ Section12.Transactions/Program.cs
@@ -1,9 +1,64 @@
 ﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
 using Section12.Transactions.DAL;
+using System.Data;
+using System.Data.Common;
 
-using (var _context = new AppDbContext())
+Initializer.Build();
+
+var connection = new SqlConnection(Initializer.Configuration.GetConnectionString("SqlCon"));
+
+IDbContextTransaction transaction = null;
+
+
+
+using (var _context = new AppDbContext(connection))
 {
 
+    using (transaction = _context.Database.BeginTransaction())
+    {
+        var category = new Category() { Name = "Telefonlar" };
+
+        _context.Categories.Add(category);
+
+        _context.SaveChanges();
+
+
+        var product = _context.Products.First();
+
+        product.Name = "Defter 123";
+
+
+        _context.SaveChanges();
+
+
+        using (var dbContext2 = new AppDbContext(connection))
+        {
+
+            dbContext2.Database.UseTransaction(transaction.GetDbTransaction());
+
+            var product2 = dbContext2.Products.First();
+
+            product2.Stock = 100;
+            dbContext2.SaveChanges();
+
+        }
+
+
+        transaction.Commit();
+
+    }
+
+    Console.WriteLine();
+
+
+    ///---------------------------------------------------
+
+
+    /*
     using (var transaction = _context.Database.BeginTransaction())
     {
         var category = new Category() { Name = "Telefonlar" };
@@ -25,7 +80,7 @@ using (var _context = new AppDbContext())
 
         transaction.Commit(); //Bir işlem hata alsa bile tüm save changeler geri alınıyor.
     }
-
+    */
 
 
     //-----------------------------------------------------
