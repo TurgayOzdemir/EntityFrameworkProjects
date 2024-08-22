c4ff941bd239a120043e803cfdcda8e29d33e26a
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-22 19:19:50 +0300

    Transaction

--- Section12.Transactions/Program.cs
+++ Section12.Transactions/Program.cs
@@ -4,6 +4,35 @@ using Section12.Transactions.DAL;
 using (var _context = new AppDbContext())
 {
 
+    using (var transaction = _context.Database.BeginTransaction())
+    {
+        var category = new Category() { Name = "Telefonlar" };
+
+        _context.Categories.Add(category);
+
+        _context.SaveChanges();
+
+
+        throw new Exception();
+
+
+        var product = _context.Products.First();
+
+        product.Name = "Defter 123";
+
+
+        _context.SaveChanges();
+
+        transaction.Commit(); //Bir işlem hata alsa bile tüm save changeler geri alınıyor.
+    }
+
+
+
+    //-----------------------------------------------------
+
+
+
+    /*
     var category = new Category() { Name = "Telefonlar" };
 
     _context.Categories.Add(category);
@@ -20,4 +49,5 @@ using (var _context = new AppDbContext())
     _context.SaveChanges();
 
     Console.WriteLine();
+    */
 }
