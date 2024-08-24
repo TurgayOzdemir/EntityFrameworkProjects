7ae67f1f78d35152c88f0c5673c2591c8b2c2450
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-24 19:08:03 +0300

    DbUpdateConcurrencyException Exception

--- Section14.Concurrency.Web/Controllers/ProductsController.cs
+++ Section14.Concurrency.Web/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Section14.Concurrency.Web.Models;
+using System.Data;
 
 namespace Section14.Concurrency.Web.Controllers
 {
@@ -23,10 +24,36 @@ namespace Section14.Concurrency.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(Product product)
         {
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(List));
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First();
+
+                var currentProduct = exceptionEntry.Entity as Product;
+                var databaseValues = exceptionEntry.GetDatabaseValues();
+                
+                var clientValues = exceptionEntry.CurrentValues;
+
+                if (databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından silindi.");
+                }
+                else
+                {
+                    var databaseProduct = databaseValues.ToObject() as Product;
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından güncellendi.");
+                    ModelState.AddModelError(string.Empty, $"Güncellenen Değer: Name:{databaseProduct.Name}, Price {databaseProduct.Price}, Stock: {databaseProduct.Stock}");
+                }
+                
+                return View(product);
+            }
+            
         }
 
         public async Task<IActionResult> List()
