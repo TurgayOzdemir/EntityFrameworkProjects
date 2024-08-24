ae2768a2dadd93f985e7eaef3af8b3fdafa02329
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-24 12:54:28 +0300

    Serializable

--- Section13.Isolation/Program.cs
+++ Section13.Isolation/Program.cs
@@ -5,7 +5,7 @@ using Section13.Isolation.DAL;
 using (var _context = new AppDbContext())
 {
 
-    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead)) //Okuma yaparken Ypdate delete gibi işlemler yapılamaz.
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable)) //Okuma yaparken Insert delete update gibi işlemler yapılamaz.
     {
         var products = _context.Products.Take(2).ToList();
 
@@ -13,34 +13,50 @@ using (var _context = new AppDbContext())
     }
 
 
+
+    //------------------------------------------------------------------
+
+
+
+    /*
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead)) //Okuma yaparken delete update gibi işlemler yapılamaz.
+    {
+        var products = _context.Products.Take(2).ToList();
+
+        transaction.Commit();
+    }
+    */
+
+
+
     //--------------------------------------------------------------------
 
 
-        /*
-        using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
-        {
-            var product = _context.Products.First();
-            product.Price = 1000;
-            _context.SaveChanges();
+    /*
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
+    {
+        var product = _context.Products.First();
+        product.Price = 1000;
+        _context.SaveChanges();
 
-            transaction.Commit();
-        }
-        */
-
-
-
-        //------------------------------------------------------------------------
+        transaction.Commit();
+    }
+    */
 
 
-        /*
-        using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
-        {
-            var product = _context.Products.First();
-            product.Price = 3000;
-            _context.SaveChanges();
 
-            transaction.Commit();
-            Console.WriteLine();
-        }
-        */
+    //------------------------------------------------------------------------
+
+
+    /*
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
+    {
+        var product = _context.Products.First();
+        product.Price = 3000;
+        _context.SaveChanges();
+
+        transaction.Commit();
+        Console.WriteLine();
+    }
+    */
 }
