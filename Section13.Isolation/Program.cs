e76a5b3b899809b59f62e53a693201c7654eb0bb
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-24 14:09:33 +0300

    Snapshot

--- Section13.Isolation/Program.cs
+++ Section13.Isolation/Program.cs
@@ -5,13 +5,28 @@ using Section13.Isolation.DAL;
 using (var _context = new AppDbContext())
 {
 
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.Snapshot))
+    {
+        var products1 = _context.Products.AsNoTracking().ToList(); ;
+
+        var products2 = _context.Products.AsNoTracking().ToList(); ;
+
+        transaction.Commit();
+    }
+
+
+
+    //-----------------------------------------------------------------
+
+
+    /*
     using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable)) //Okuma yaparken Insert delete update gibi işlemler yapılamaz.
     {
         var products = _context.Products.Take(2).ToList();
 
         transaction.Commit();
     }
-
+    */
 
 
     //------------------------------------------------------------------
