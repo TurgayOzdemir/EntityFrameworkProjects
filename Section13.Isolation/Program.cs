10157a8977ca8d47aa45327e225db7b31407d818
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-08-24 12:51:27 +0300

    Repeatable read

--- Section13.Isolation/Program.cs
+++ Section13.Isolation/Program.cs
@@ -5,28 +5,42 @@ using Section13.Isolation.DAL;
 using (var _context = new AppDbContext())
 {
 
-    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
+    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead)) //Okuma yaparken Ypdate delete gibi işlemler yapılamaz.
     {
-        var product = _context.Products.First();
-        product.Price = 1000;
-        _context.SaveChanges();
+        var products = _context.Products.Take(2).ToList();
 
         transaction.Commit();
     }
 
 
-    //------------------------------------------------------------------------
+    //--------------------------------------------------------------------
 
 
-    /*
-    using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
-    {
-        var product = _context.Products.First();
-        product.Price = 3000;
-        _context.SaveChanges();
+        /*
+        using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
+        {
+            var product = _context.Products.First();
+            product.Price = 1000;
+            _context.SaveChanges();
 
-        transaction.Commit();
-        Console.WriteLine();
-    }
-    */
+            transaction.Commit();
+        }
+        */
+
+
+
+        //------------------------------------------------------------------------
+
+
+        /*
+        using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
+        {
+            var product = _context.Products.First();
+            product.Price = 3000;
+            _context.SaveChanges();
+
+            transaction.Commit();
+            Console.WriteLine();
+        }
+        */
 }
